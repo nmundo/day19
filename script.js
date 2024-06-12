f06cbb0198982d0b70e028a73890403e15b927d2
Nathan Mundo <nathanmundo@icloud.com>
2024-06-12 18:36:00 +0000

    initial

---
+++ script.js
@@ -0,0 +1,13 @@
+// Arrow Functions
+// These 3 all do the same
+
+const add1 = function(arg1, arg2) {
+  return arg1 + arg2;
+}
+
+const add2 = (arg1, arg2) => {
+  return arg1 + arg2;
+}
+
+const add3 = (arg1, arg2) => arg1 + arg2;
+
